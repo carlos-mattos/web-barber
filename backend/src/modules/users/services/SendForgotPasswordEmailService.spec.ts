34fb0626a10de3f93ce82b594e329a44cd024eb8
carlos-mattos <ceamattos.jf@gmail.com>
2021-05-03 14:03:08 -0300

    testando email pt1

---
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,30 @@
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import AppError from '@shared/errors/AppError';
+import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+
+describe('SendForgotPasswordEmail', () => {
+  it('should be able to recover password using email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+
+    const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider
+    );
+
+    await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    await sendForgotPasswordEmail.execute({
+      email: 'johndoe@example.com',
+    });
+
+    expect(sendMail).toHaveBeenCalled();
+  });
+});

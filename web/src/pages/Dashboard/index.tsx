da17b871118f38adf511dbbcb2d8821d7767b678
carlos-mattos <ceamattos.jf@gmail.com>
2021-05-07 15:05:44 -0300

    dashboard

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,10 +1,123 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+  Section,
+  Appointment,
+} from './styles';
+import logoImg from '../../assets/logo.svg';
+import { FiClock, FiPower } from 'react-icons/fi';
+import { useAuth } from '../../hooks/AuthContext';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const { signOut, user } = useAuth();
+
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="logoGoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-Feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars.githubusercontent.com/u/66537139?v=4"
+                alt="avatar"
+              />
+
+              <strong>Carlos Eduardo</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+            <Appointment>
+              <span>
+                <FiClock /> 08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/66537139?v=4"
+                  alt="avatar"
+                />
+
+                <strong>Carlos Eduardo</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock /> 08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/66537139?v=4"
+                  alt="avatar"
+                />
+
+                <strong>Carlos Eduardo</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+            <Appointment>
+              <span>
+                <FiClock /> 08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/66537139?v=4"
+                  alt="avatar"
+                />
+
+                <strong>Carlos Eduardo</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
+    </Container>
   );
 };
 
